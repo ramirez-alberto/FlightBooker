6888a2d48f9e41d8bc74d895e363d0f56c01915a
ramirez-alberto <ramirez.alberto@zoho.com>
2023-01-17 18:33:40 -0800

    Update Airport model

--- Entities/Models/Airport.cs
+++ Entities/Models/Airport.cs
@@ -1,9 +1,20 @@
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 namespace Entities.Models;
 
-
+[Table("airport")]
 public class Airport
 {
     public int AirportId {get;set;}
+    
+    [Required]
     public string Name {set;get;}
+    
+    [Required]
     public int CityId {get;set;}
+
+    [Required]
+    [StringLength(4)]
+    public string AirportCode {get;set;}
 }
